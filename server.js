7172f157b9d205b26a0571f50e59da06a40a3532
Diego Cruz <31012485+diego-lcruz@users.noreply.github.com>
2025-11-16 19:28:39 -0300

    Fix formatting in server.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, 'public'))); // ✅ Modifique esta linha
+app.use(express.static(path.join(__dirname, 'public'))); 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html')); // ✅ Modifique esta linha
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
